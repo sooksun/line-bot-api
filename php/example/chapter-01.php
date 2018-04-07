83bf4214d03e9c9ba2411c4311c97d4592d8a9e9
School e office <sooksun2511@gmail.com>
2018-04-08 06:51:11 +0700

    Update chapter-01.php

--- php/example/chapter-01.php
+++ php/example/chapter-01.php
@@ -2,8 +2,8 @@
 
 include ('line-bot-api/php/line-bot.php');
 
-$channelSecret = '551ec4feee0.....43cff0';
-$access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '3163eae7704dfcf9894d608ca489bc32';
+$access_token  = 'c7W/6apDMEwyg2Px5x6dRnT4uvzxBY+ESRIigI57kOY/dvyR0Wbi6SG6jPGJTLvTt35PlSuoBuBjeDvSbJCWAfcpiz1ErocHOOO25kNVOQcDdT9yW1aWTG73cm/tuzqWKv//M1kBOS1lRW6UYVIY8gdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
